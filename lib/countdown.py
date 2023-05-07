6027b82e88670ae3e0098e7de6dd68c901979c14
+ <+>
2023-05-06 23:34:02 -0400

    satisfy deliverables

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,17 @@
-# your code goes here!
+import time
+
+def countdown(nummy):
+    while nummy != 0:
+        print(f"{nummy} SECOND(S)!")
+        nummy -= 1 
+        if nummy == 0: 
+            print("HAPPY NEW YEAR!")
+
+
+def countdown_with_sleep(nummy):
+    while nummy != 0:
+        print(f"{nummy} SECOND(S)!")
+        nummy -= 1
+        time.sleep(1)
+        if nummy == 0: 
+            print("HAPPY NEW YEAR!")
